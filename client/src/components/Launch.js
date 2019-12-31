b82903ca9b2a0ef6fe454a6b8765b1f0b137fff0
Vita <vitalia@myrambler.ru>
2019-12-31 17:05:59 +0300

    + launch component layout

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -33,7 +33,57 @@ export class Launch extends Component {
             if (error) console.log(error);
             console.log(data);
 
-            return <h1>test</h1>;
+            const {
+              mission_name,
+              flight_number,
+              launch_year,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type }
+            } = data.launch; // destructuring properties from data object
+
+            return (
+              <div>
+                <h1 className="display-4 my-3">
+                  <span className="text-dark">Mission:</span> {mission_name}
+                </h1>
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number: {flight_number}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Year: {launch_year}
+                  </li>
+                  <li className="list-group-item">
+                    Launch Successful:{" "}
+                    <span // using classnames for firing up classnames based on condition
+                      className={classNames({
+                        "text-success": launch_success,
+                        "text-danger": !launch_success
+                      })}
+                    >
+                      {launch_success ? "Yes" : "No"}
+                    </span>
+                  </li>
+                </ul>
+
+                {/*data object was already desctuctured above*/}
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">
+                    Rocket Name: {rocket_name}
+                  </li>
+                  <li className="list-group-item">
+                    Rocket Type: {rocket_type}
+                  </li>
+                </ul>
+                <hr />
+                <Link to="/" className="btn btn-secondary">
+                  Back
+                </Link>
+              </div>
+            );
           }}
         </Query>
       </Fragment>
