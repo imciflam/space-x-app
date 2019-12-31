3af2aaa9e3904a2cec536f085dbfde8ef482fe18
Vita <vitalia@myrambler.ru>
2019-12-31 17:11:04 +0300

    logo  is clickable now

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Launches from "./components/Launches";
 import Launch from "./components/Launch";
+import { Link } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -17,11 +18,13 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="container">
-          <img
-            src={logo}
-            alt="SpaceX"
-            style={{ width: 300, display: "block", margin: "auto" }}
-          />
+          <Link to="/">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: "block", margin: "auto" }}
+            />{" "}
+          </Link>
           <Route exact path="/" component={Launches} />
           <Route exact path="/launch/:flight_number" component={Launch} />
         </div>
