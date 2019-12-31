9783c2f59fbad0a48277d8fe83d464e48b04bb0f
Vita <vitalia@myrambler.ru>
2019-12-31 16:22:29 +0300

    + new func component for item

---
+++ client/src/components/LaunchItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+
+// func component, sending props as parameters & destructuring them
+export default function LaunchItem({
+  launch: { flight_number, mission_name, launch_date_local, launch_success }
+}) {
+  return (
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+          <h4>
+            Mission:{" "}
+            <span
+              className={classNames({
+                "text-success": launch_success,
+                "text-danger": !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
+          <p>Date:</p>
+        </div>
+        <div className="col-md-3">Launch Details</div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import LaunchItem from "./LaunchItem";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -22,7 +23,14 @@ export class Launches extends Component {
           {({ loading, error, data }) => {
             if (loading) return <h4>Loading...</h4>;
             if (error) console.log(error);
-            return <Fragment>test</Fragment>;
+            // can console log all the data here
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+              </Fragment>
+            );
           }}
         </Query>
       </Fragment>
